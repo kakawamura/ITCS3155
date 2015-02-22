9b7617c73c9e396a9c0ecaae97732918fdde39a6
waka-ka <saasbook@saasbook.(none)>
2015-02-22 16:57:32 -0400

    add my rotten potatoes rails application

---
+++ railsapp/myrottenpotatoes/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myrottenpotatoes::Application.config.secret_token = 'c599fa4c2701fff53f217f309a4df37d324a3e184bfd616f3a8f3f5914b5b8967d0b39901e15eb3838260242fbed62a8f88ddbb58c871fdb65ff295fb653997f'
