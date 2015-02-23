83ec728600546f8b000f2868641a57cbc24b3f43
waka-ka <wakaka2277@gmail.com>
2015-02-23 15:56:32 -0400

    finish chapter 4

--- railsapp/myrottenpotatoes/app/controllers/movies_controller.rb
+++ railsapp/myrottenpotatoes/app/controllers/movies_controller.rb
@@ -1,5 +1,6 @@
 # movie_controller.rb
 class MoviesController < ApplicationController
+
   def index
     @movies = Movie.all
   end
@@ -17,6 +18,24 @@ class MoviesController < ApplicationController
     # debugger
     @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
+    redirect_to movies_path
+  end
+
+  def edit
+    @movie = Movie.find(params[:id]) 
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was succesfully updated"
+    redirect_to movie_path(@movie)
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.destroy
+    flash[:notice] = "Movie #{@movie.title} was deleted"
     redirect_to movies_path
   end
 end
